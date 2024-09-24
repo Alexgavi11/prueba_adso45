d7cb536d63bb1a008b08e2796894dc5719858e6d
Eduardo Ealo <eealo49@gmail.com>
2024-09-24 01:17:10 -0500

    corrigiendo

--- frontend/proyecto/src/Login.jsx
+++ frontend/proyecto/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './login.css'
+import './Login.css'
 import { Link } from "react-router-dom";
 import Google from './assets/Google.png';
 import Apple from './assets/Apple.png';
